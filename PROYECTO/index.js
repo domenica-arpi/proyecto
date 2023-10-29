7f95c3ab93e19a65c6aa011f717514bfebec72e8
domenica-arpi <133069829+domenica-arpi@users.noreply.github.com>
2023-10-28 20:20:59 -0500

    Add files via upload

---
+++ PROYECTO/index.js
@@ -0,0 +1,34 @@
+var inputTarea = document.getElementById("inputTarea");
+var listaTarea = document.getElementById("listaTarea");
+
+inputTarea.addEventListener("keydown", function(event){
+    if(event.key === "Enter" && inputTarea.value){
+        agregarTarea(inputTarea.value);
+        inputTarea.value = "";
+    };
+});
+
+function agregarTarea(textoTarea) {
+    var li = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function(){
+        li.classList.toggle("completed");
+    });
+
+    var texto = document.createElement("p");
+    texto.innerHTML = textoTarea;
+    var botonEliminar = document.createElement("span");
+    botonEliminar.classList.add("delete");
+    botonEliminar.innerHTML = "<ion-icon name='trash-outline'></ion-icon>"
+    
+    botonEliminar.addEventListener("click", function(){
+        li.parentNode.removeChild(li);
+    });
+
+
+    li.appendChild(checkbox);
+    li.appendChild(texto);
+    li.appendChild(botonEliminar);
+    listaTarea.appendChild(li);
+}
